2c0e6465e8681f21187cd4e49c75733b0dee6fbf
la <hack.xiyuan@gmail.com>
2025-01-09 02:41:31 +0800

    cleanup vitepress config

--- packages/docs/.vitepress/config.ts
+++ packages/docs/.vitepress/config.ts
@@ -12,35 +12,6 @@ export default defineConfig({
   markdown: {
     config: (md) => {
       md.use(groupIconMdPlugin)
-      const defaultRender = md.renderer.rules.image
-      // image alias
-      md.renderer.rules.image = (
-        tokens,
-        idx: number,
-        options,
-        env: any,
-        self
-      ) => {
-        const token = tokens[idx];
-        const attrs = token.attrs;
-
-        if (!attrs) {
-          return defaultRender!(tokens, idx, options, env, self);
-        }
-
-        const srcIndex = attrs.findIndex(([attr]) => attr === 'src');
-        if (srcIndex >= 0) {
-          const [attr, value] = attrs[srcIndex];
-
-          // 确保路径是正确的
-          if (value.startsWith('@oss/')) {
-            const resolvedPath = value.replace(/^@oss\//, ''); // 移除前缀
-            attrs[srcIndex][1] = `${BASE_PATH}oss/${resolvedPath}`; // 拼接相对路径
-          }
-        }
-
-        return self.renderToken(tokens, idx, options);
-      };
     }
 
   },
